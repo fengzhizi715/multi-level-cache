e4685d5c6a2f00515c1dc5699703f642909c26fe
fengzhizi715 <fengzhizi715@126.com>
2018-04-06 20:25:37 +0800

    add CacheTypeEnum

--- src/main/java/com/cv4j/cacheadmin/cache/ICache.java
+++ src/main/java/com/cv4j/cacheadmin/cache/ICache.java
@@ -15,43 +15,7 @@ public interface ICache {
      * @return
      */
     String get(String key);
-
-    /**
-     * 保存缓存
-     *
-     * @param key
-     * @param value
-     * @param expireMin
-     *
-     */
-    void set(String key, Object value, int expireMin);
-
-    /**
-     * 保存缓存
-     *
-     * @param key
-     * @param value
-     * @param expireMin
-     * @param desc
-     */
-    void set(String key, Object value, int expireMin, String desc);
-
-    /**
-     * 移除单个缓存
-     *
-     * @param key
-     * @return
-     */
-    void remove(String key);
-
-    /**
-     * 移除多个缓存
-     *
-     * @param keys
-     * @return
-     */
-    void remove(String... keys);
-
+    
     /**
      * 获取所有缓存对象信息
      *
@@ -60,4 +24,40 @@ public interface ICache {
      * @date 2017年5月12日
      */
     List<CacheEntity> listAll();
+
+    /**
+     * 保存缓存
+     *
+     * @param key
+     * @param value
+     * @param expireTime
+     *
+     */
+    void set(String key, Object value, int expireTime);
+
+    /**
+     * 保存缓存
+     *
+     * @param key
+     * @param value
+     * @param expireTime
+     * @param desc
+     */
+    void set(String key, Object value, int expireTime, String desc);
+
+    /**
+     * 删除单个缓存
+     *
+     * @param key
+     * @return
+     */
+    void remove(String key);
+
+    /**
+     * 删除多个缓存
+     *
+     * @param keys
+     * @return
+     */
+    void remove(String... keys);
 }
---
+++ src/main/java/com/cv4j/cacheadmin/domain/CacheTypeEnum.java
@@ -0,0 +1,11 @@
+package com.cv4j.cacheadmin.domain;
+
+/**
+ * Created by tony on 2018/4/6.
+ */
+public enum CacheTypeEnum {
+
+    MEMCACHED,
+    REDIS,
+    COUCHBASE;
+}
