3b940892b73cad02fdf8ed0af0df516425b11fe7
fengzhizi715 <fengzhizi715@126.com>
2019-01-15 17:51:28 +0800

    add Configuration

---
+++ cache-core/src/main/java/cn/netdiscovery/cache/config/Configuration.java
@@ -0,0 +1,41 @@
+package cn.netdiscovery.cache.config;
+
+import cn.netdiscovery.cache.common.YamlParser;
+import com.safframework.tony.common.utils.Preconditions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by tony on 2019-01-15.
+ */
+public class Configuration {
+
+    private static YamlParser yamlParser;
+    private static Map<String,Object> configs = new HashMap<>();
+
+    static {
+
+        yamlParser = new YamlParser(".");
+        try {
+            Map<String,Object> yaml = yamlParser.decode(Configuration.class.getResourceAsStream("/application.yaml"));
+            if(Preconditions.isNotBlank(yaml)) {
+                configs.putAll(yaml);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Set<String> keys() {
+
+        return configs.keySet();
+    }
+
+    public static Object get(String key) {
+
+        return configs.get(key);
+    }
+}
